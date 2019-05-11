80fbcab9ca92bd30534cf6122a03cc7170c4cd7d
adanli <adanli@126.com>
2019-05-11 16:23:16 +0800

    初始化项目

--- src/main/java/com/egg/ih/demo/control/DemoController.java
+++ src/main/java/com/egg/ih/demo/control/DemoController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author Administrator
+ */
 @RequestMapping(value = "/demo")
 @RestController
 public class DemoController {
