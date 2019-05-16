ecb99067d3cac86c89c809431356812bed0021c4
adanli <adanli@126.com>
2019-05-16 22:54:45 +0800

    添加controller层接口，数据库映射

--- src/main/java/com/egg/ih/biz/api/control/ApiController.java
+++ src/main/java/com/egg/ih/biz/api/control/ApiController.java
@@ -32,10 +32,10 @@ public class ApiController {
     @ApiOperation(notes = "/interface", value = "创建接口")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "interfaceVO", value = "接口类", dataType = "IhInterfaceVO", paramType = "query", required = true),
-            @ApiImplicitParam(name = "paramsVO", value = "params参数", dataType = "IhParamsVO", paramType = "query", required = true),
-            @ApiImplicitParam(name = "headerVO", value = "headers参数", dataType = "IhHeaderVO", paramType = "query", required = true),
-            @ApiImplicitParam(name = "bodyVO", value = "body参数", dataType = "IhBodyVO", paramType = "query", required = true),
-            @ApiImplicitParam(name = "responseVO", value = "返回值类", dataType = "IhResponseVO", paramType = "query", required = true)
+            @ApiImplicitParam(name = "paramsVO", value = "params参数", dataType = "IhParamsVO", paramType = "query"),
+            @ApiImplicitParam(name = "headerVO", value = "headers参数", dataType = "IhHeaderVO", paramType = "query"),
+            @ApiImplicitParam(name = "bodyVO", value = "body参数", dataType = "IhBodyVO", paramType = "query"),
+            @ApiImplicitParam(name = "responseVO", value = "返回值类", dataType = "IhResponseVO", paramType = "query")
     })
     @PostMapping(value = "/interface")
     public BaseResponse createInterface(@RequestBody IhInterfaceVO interfaceVO,
@@ -77,18 +77,54 @@ public class ApiController {
         return ResponseBuilder.build(DefaultErrorCode.SUCCESS, new IhClassVO());
     }
 
-    @ApiOperation(notes = "/class/{classId}", value = "根据类主键删除类")
+    @ApiOperation(notes = "/class/{classId}", value = "根据类主键删除接口类")
     @ApiImplicitParam(name = "classId", value = "接口主键", dataType = "String", paramType = "path", required = true)
     @DeleteMapping(value= "/class/{classId}")
     public BaseResponse deleteClassById(@PathVariable String classId) {
         return ResponseBuilder.build(DefaultErrorCode.SUCCESS);
     }
 
-    @ApiOperation(notes = "/interface/{interfaceId}", value = "根据类主键删除类")
+    @ApiOperation(notes = "/interface/{interfaceId}", value = "根据类主键删除接口")
     @ApiImplicitParam(name = "interface", value = "接口主键", dataType = "String", paramType = "path", required = true)
     @DeleteMapping(value= "/interface/{interfaceId}")
     public BaseResponse deleteInterfaceById(@PathVariable String interfaceId) {
         return ResponseBuilder.build(DefaultErrorCode.SUCCESS);
     }
+
+    @ApiOperation(notes = "/class/{classId}", value = "创建接口类")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "classId", value = "接口类主键", dataType = "String", paramType = "path", required = true),
+        @ApiImplicitParam(name = "className", value = "接口类名称", dataType = "String", paramType = "query", required = true)
+    })
+    @PutMapping(value = "/class/{classId}")
+    public BaseResponse updateClass(@PathVariable String classId, @RequestParam String className) {
+        return ResponseBuilder.build(DefaultErrorCode.SUCCESS);
+    }
+
+    @ApiOperation(notes = "/interface/{interfaceId}", value = "创建接口")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "interfaceId", value = "接口主键", dataType = "String", paramType = "path", required = true),
+            @ApiImplicitParam(name = "interfaceVO", value = "接口类", dataType = "IhInterfaceVO", paramType = "query", required = true),
+            @ApiImplicitParam(name = "paramsVO", value = "params参数", dataType = "IhParamsVO", paramType = "query"),
+            @ApiImplicitParam(name = "headerVO", value = "headers参数", dataType = "IhHeaderVO", paramType = "query"),
+            @ApiImplicitParam(name = "bodyVO", value = "body参数", dataType = "IhBodyVO", paramType = "query"),
+            @ApiImplicitParam(name = "responseVO", value = "返回值类", dataType = "IhResponseVO", paramType = "query")
+    })
+    @PutMapping(value = "/interface/{interfaceId}")
+    public BaseResponse updateInterfaceById(@PathVariable String interfaceId,
+                                        @RequestBody IhInterfaceVO interfaceVO,
+                                        @RequestBody(required = false) IhParamsVO paramsVO,
+                                        @RequestBody(required = false) IhHeaderVO headerVO,
+                                        @RequestBody(required = false) IhBodyVO bodyVO,
+                                        @RequestBody(required = false) IhResponseVO responseVO) {
+        return ResponseBuilder.build(DefaultErrorCode.SUCCESS);
+    }
+
+    @ApiOperation(notes = "/interfaces/history", value = "查询记录访问的操作的历史记录")
+    @GetMapping(value = "/interfaces/history")
+    public BaseResponse<List<IhHiOperVO>> findHistory() {
+        return ResponseBuilder.build(DefaultErrorCode.SUCCESS, new ArrayList<>());
+    }
+
 
 }
---
+++ src/main/java/com/egg/ih/biz/api/vo/IhHiOperVO.java
@@ -0,0 +1,10 @@
+package com.egg.ih.biz.api.vo;
+
+import lombok.Data;
+
+/**
+ * @author Administrator
+ */
+@Data
+public class IhHiOperVO {
+}
